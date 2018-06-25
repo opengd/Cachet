9393886b4cf7ad481d3720953de95f2cdde83428
opengd <opengd@outlook.com>
2018-06-25 15:58:17 +0200

    First try on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-12">
                 @if($show_support)
                 <p>
                     {!! trans('cachet.powered_by') !!}
@@ -15,6 +15,9 @@
                 @endif
             </div>
             <div class="col-sm-8">
+                <h4>{{ trans('cachet.get_subscribe') }}</h4>
+            </div>
+            <div class="col-sm-4">
                 <ul class="list-inline">
                     @if($current_user || $dashboard_link)
                     <li>
@@ -26,12 +29,6 @@
                         <a class="btn btn-link" href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
                     </li>
                     @endif
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('feed.rss') }}">{{ trans('cachet.rss-feed') }}</a>
-                    </li>
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('feed.atom') }}">{{ trans('cachet.atom-feed') }}</a>
-                    </li>
                     @if($enable_subscribers)
                     <li>
                         <a class="btn btn-success btn-outline" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
