6d30c0f06b3d32280ce7289cc046cedfdff5eb62
opengd <opengd@outlook.com>
2018-06-27 11:19:18 +0200

    Moved styles to css

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,6 @@
 <tr>
     <td class="header">
-        <p style="text-align: center;">
-        <img src="data:{{ $app_banner_type }};base64, {{ $app_banner }}" style="text-align: center; width=75px; height: 75px">
-        </p>
+        <p><img src="data:{{ $app_banner_type }};base64, {{ $app_banner }}"></p>
         <a href="{{ $url }}">
             {{ $slot }}
         </a>
