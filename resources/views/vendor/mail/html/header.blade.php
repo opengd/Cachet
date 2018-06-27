8fdd0d71092ba5c77e4fe0806de56ea5090aae63
opengd <opengd@outlook.com>
2018-06-27 11:04:23 +0200

    Added logo to email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,8 @@
 <tr>
     <td class="header">
+        <p style="text-align: center;">
+        <img src="data:{{ $app_banner_type }};base64, {{ $app_banner }}" style="text-align: center; width=75px; height: 75px">
+        </p>
         <a href="{{ $url }}">
             {{ $slot }}
         </a>
