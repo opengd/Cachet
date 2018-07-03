024e21068acb14e042561a1c979d826c9729bab8
opengd <opengd@outlook.com>
2018-07-03 10:31:21 +0200

    Change past to ongoing

--- resources/views/partials/modules/timeline.blade.php
+++ resources/views/partials/modules/timeline.blade.php
@@ -9,7 +9,7 @@ $total_incidents += count($incidents);
 @endforeach
 @if($total_incidents > 0)
 <div class="section-timeline">
-    <h1>{{ trans('cachet.incidents.past') }}</h1>
+    <h1>{{ trans('cachet.incidents.ongoing') }}</h1>
     @foreach($allIncidents as $date => $incidents)
     @include('partials.incidents', [compact($date), compact($incidents)])
     @endforeach
