6100d60f2c9efcebe3293c75aff3c5dc41765b98
opengd <opengd@outlook.com>
2018-07-19 09:46:12 +0200

    Fixed settings vars names for banner image url

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,20 +1,20 @@
 @if($appHeader)
 {!! $appHeader !!}
 @else
-@if($appBanner)
+@if($appBanner || Config::get('setting.banner-image-url'))
 <div @if($appBannerStyleFullWidth)class="app-banner"@endif>
     <div class="container">
         <div class="row app-banner-padding  @if(!$appBannerStyleFullWidth) app-banner @endif">
             <div class="col-md-12 text-center">
                 @if($appDomain)
-                    @if(Config::get('setting.header-image'))
-                    <a href="{{ $appDomain }}" class="links"><img src="{{ Config::get('setting.header-image') }}" class="banner-image img-responsive"></a>
+                    @if(Config::get('setting.banner-image-url'))
+                    <a href="{{ $appDomain }}" class="links"><img src="{{ Config::get('setting.banner-image-url') }}" class="banner-image img-responsive"></a>
                     @else
                     <a href="{{ $appDomain }}" class="links"><img src="data:{{ $appBannerType }};base64, {{ $appBanner }}" class="banner-image img-responsive"></a>
                     @endif
                 @else
-                    @if(Config::get('setting.header-image'))
-                    <img src="{{ Config::get('setting.header-image') }}" class="banner-image img-responsive">
+                    @if(Config::get('setting.banner-image-url'))
+                    <img src="{{ Config::get('setting.banner-image-url') }}" class="banner-image img-responsive">
                     @else
                     <img src="data:{{ $appBannerType }};base64, {{ $appBanner }}" class="banner-image img-responsive">
                     @endif
