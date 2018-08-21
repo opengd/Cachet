205c56a3d1ff5e99865c75f42b38b4b024855d2b
opengd <opengd@outlook.com>
2018-08-21 16:25:47 +0200

    Fix button text is now using slot and not hardcoded

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,5 +1,5 @@
 <div class="view">
     <center>
-        <a style="color: #ffffff; text-decoration: none; background-color: #5081A0; text-transform: uppercase;" href="{{ $url }}" class="button button-{{ $color or 'blue' }}" target="_blank">View</a>
+        <a style="color: #ffffff; text-decoration: none; background-color: #5081A0; text-transform: uppercase;" href="{{ $url }}" class="button button-{{ $color or 'blue' }}" target="_blank">{{ $slot }}</a>
     </center>
 </div>
